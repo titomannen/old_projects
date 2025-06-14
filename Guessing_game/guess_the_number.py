7a194db20942372210510b5d34f0058f739d087d
rafa2 <124042780+titomannen@users.noreply.github.com>
2025-06-15 01:23:46 +0200

    Update guess_the_number.py

--- Guessing_game/guess_the_number.py
+++ Guessing_game/guess_the_number.py
@@ -1,1 +1,38 @@
+#2023-10-13 school work
+#simple guessing game that works
+#i uploaded this file to test out github features
 
+import random
+secret_number = random.randint(1, 1000)
+
+def secret_number_loop(guess, tries):
+    if guess == secret_number:
+        print("You have guessed the correct number and it took you", tries, "tries!")
+        return True
+    elif guess < secret_number:
+        print("You have guessed too low")
+    else:
+        print("You have guessed too high")
+    return False
+
+def game():
+    print("Welcome to the number guessing game!")
+    guess = None
+    tries = 0
+    while guess != secret_number:
+        guess = int(input("Guess the number between 1-100:"))
+        tries += 1
+        if secret_number_loop(guess, tries):
+            break
+
+def main():
+    while True:
+        answer = input("Do you want to play the guessing game?").strip().lower()
+        if answer == "yes":
+            game()
+        elif answer == "no":
+            print("Oh no, what a bummer!")
+            break
+        else:
+            print("please answer with yes or no")
+main()
